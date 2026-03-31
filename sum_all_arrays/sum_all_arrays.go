7d7c67bdda4318ccad1dda872e57cbb10e00088c
andrei <andrei@ciocoiu.net>
2026-03-31 15:22:04 +0300

    Refactor SumAllArrays

--- sum_all_arrays/sum_all_arrays.go
+++ sum_all_arrays/sum_all_arrays.go
@@ -2,11 +2,11 @@ package sumallarrays
 
 // Will return a slice with each element being the sum of a slice that was provided
 func SumAllArrays(numbersToSum ...[]int) []int {
-	nrOfSums := len(numbersToSum)
-	sums := make([]int, nrOfSums)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = ArrSum(numbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, ArrSum(numbers))
 	}
 
 	return sums
