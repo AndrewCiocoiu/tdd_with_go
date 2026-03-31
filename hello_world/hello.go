856799b029abca70c369351c1c5e9314f5dfebd5
andrei <andrei@ciocoiu.net>
2026-03-31 10:43:15 +0300

    Add spanish language support

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,17 +3,22 @@ package main
 import "fmt"
 
 const helloEnglishPrefix = "Hello, "
+const helloSpanishPreifx = "Hola, "
 const defaultGreeting = "Hello, Stranger"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 
 	if name == "" {
 		return defaultGreeting
+	}
+
+	if language == "spanish" {
+		return helloSpanishPreifx + name
 	}
 
 	return helloEnglishPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,15 +4,21 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people.", func(t *testing.T) {
-		got := Hello("Andrei")
+		got := Hello("Andrei", "")
 		want := "Hello, Andrei"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying stranger to people who don't provide their name.", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, Stranger"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Greet in Spanish", func(t *testing.T) {
+		got := Hello("Andrew", "spanish")
+		want := "Hola, Andrew"
 		assertCorrectMessage(t, got, want)
 	})
 }
