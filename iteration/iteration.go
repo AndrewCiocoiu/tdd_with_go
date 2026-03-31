a19a89f85360cd1046ec645b750a05722ba02d68
andrei <andrei@ciocoiu.net>
2026-03-31 12:25:31 +0300

    Improve performance of Iteration function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,12 +1,14 @@
 package iteration
 
+import "strings"
+
 // Repeat will repeat the given string 5 times
 func Repeat(s string) string {
-	var repeated string
+	var repeated strings.Builder
 
 	for i := 0; i < 5; i++ {
-		repeated += s
+		repeated.WriteString(s)
 	}
 
-	return repeated
+	return repeated.String()
 }
