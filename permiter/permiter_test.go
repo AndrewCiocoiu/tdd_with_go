38d7f7d469c2c4d35d16b0eb6d447f05ba03d492
andrei <andrei@ciocoiu.net>
2026-04-02 08:40:04 +0300

    Fix permiter test to not be area test

--- permiter/permiter_test.go
+++ permiter/permiter_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestPermiter(t *testing.T) {
 	t.Run("Gives correct permiter", func(t *testing.T) {
 		got := Permiter(10.0, 10.0)
-		want := 100.0
+		want := 40.0
 
 		if got != want {
-			t.Errorf("Wanted %f, got %f", want, got)
+			t.Errorf("Wanted %.2f, got %.2f", want, got)
 		}
 	})
 }
