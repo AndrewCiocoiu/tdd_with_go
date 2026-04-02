7622b1b3e1a812d491e96a75369d967cdb483ce0
andrei <andrei@ciocoiu.net>
2026-04-02 11:59:24 +0300

    Implement circle and rectangle area method test

--- permiter/permiter.go
+++ permiter/permiter.go
@@ -5,8 +5,20 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Permiter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return 0.0
+}
+
+func (c Circle) Area() float64 {
+	return 0.0
 }
 
 func Area(rectangle Rectangle) float64 {
--- permiter/permiter_test.go
+++ permiter/permiter_test.go
@@ -15,13 +15,23 @@ func TestPermiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Gives correct area", func(t *testing.T) {
+	t.Run("Gives correct area for rectangle", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
 
 		if got != want {
 			t.Errorf("Wanted %.2f, got %.2f", want, got)
+		}
+	})
+
+	t.Run("Gives correct area for circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("Wanted %g, got %g", want, got)
 		}
 	})
 }
