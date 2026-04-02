125ad5befd738d8fa069bdd4f9f304dd3825d400
andrei <andrei@ciocoiu.net>
2026-04-02 12:11:46 +0300

    Refactor area functions and tests to use interface

--- permiter/permiter.go
+++ permiter/permiter.go
@@ -2,6 +2,10 @@ package permiter
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
--- permiter/permiter_test.go
+++ permiter/permiter_test.go
@@ -15,23 +15,23 @@ func TestPermiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if want != got {
+			t.Errorf("Got %g, wanted %g.", got, want)
+		}
+	}
+
 	t.Run("Gives correct area for rectangle", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
-
-		if got != want {
-			t.Errorf("Wanted %.2f, got %.2f", want, got)
-		}
+		checkArea(t, rectangle, 100.0)
 	})
 
 	t.Run("Gives correct area for circle", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("Wanted %g, got %g", want, got)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
