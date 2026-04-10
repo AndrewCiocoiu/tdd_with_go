70ee28e770a72977e42597dba0329c4cce6de455
andrei <andrei@ciocoiu.net>
2026-04-10 19:34:00 +0300

    Implement custom bitcoin type and make it string printable

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,21 @@
 package wallet
 
+import "fmt"
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(deposit_val int) {
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(deposit_val Bitcoin) {
 	w.balance += deposit_val
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,13 +6,13 @@ func TestWallet(t *testing.T) {
 	t.Run("Balance is deposited correctly", func(t *testing.T) {
 		wallet := Wallet{}
 
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
 		got := wallet.Balance()
-		want := 10
+		want := Bitcoin(11)
 
 		if want != got {
-			t.Errorf("Wanted %d, got %d", want, got)
+			t.Errorf("Wanted %s, got %s", want, got)
 		}
 
 	})
